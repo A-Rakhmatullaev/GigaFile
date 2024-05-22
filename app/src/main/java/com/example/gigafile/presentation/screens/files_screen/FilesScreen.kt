22ad7f0916ad1342b28dc3294c6e7ad75bb6d647
rakhmatullaev <dev.akbarbek@gmail.com>
2024-05-22 16:57:32 -0700

    - Hard-coded add button for directory: 1. Resolved errors of changing path in AddDirectoryUseCase.kt 2. Create a new directory in the current folder using 'mkdirs()' - Updated all primary icons to Material Design 2.0 icons - Added icon and corresponding icon setting logic in DirectoryViewHolder.kt - Added temporary extension-function to create temporary ID for files and directories - Created Result.kt and BaseResult.kt - Added primary and secondary colors - Created styles.xml - Added a custom ripple effect for files in FilesScreen.kt, by creating corresponding styles in styles.xml - Renamed TestRepository.kt to FileSystemRepository.kt and TestRepositoryImpl.kt to FileSystemRepositoryImpl.kt - Modified toast() and snackBar() extension-functions in Extensions.kt to extend View instead of Activity, and changed some logic inside correspondingly - Trying to create already existing folder/file within a directory (with a same name) now results in showing a Toast, which is fed by a message sent from Data layer using BaseResult.kt

--- app/src/main/java/com/example/gigafile/presentation/screens/files_screen/FilesScreen.kt
+++ app/src/main/java/com/example/gigafile/presentation/screens/files_screen/FilesScreen.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.distinctUntilChanged
@@ -15,7 +14,6 @@ import com.example.gigafile.core.bases.BaseScreen
 import com.example.gigafile.core.extensions.hide
 import com.example.gigafile.core.extensions.log
 import com.example.gigafile.core.extensions.show
-import com.example.gigafile.core.extensions.snackBar
 import com.example.gigafile.core.extensions.toast
 import com.example.gigafile.databinding.FragmentFilesBinding
 import com.example.gigafile.domain.models.core.file_system.FileSystemElement
